44002b85766f7e899edb5e0f444ca0d3ca552fc6
Abbas Musabji <amusabji@gmail.com>
2019-01-26 18:17:46 -0600

    move test files out of working directory, DriversLicenseProgram complete.

--- DriversLicense/src/DriversLicense.java
+++ DriversLicense/src/DriversLicense.java
@@ -1,3 +1,108 @@
+import java.time.*;
+
 public class DriversLicense {
+	
+	// --------------------------------------------------------- //
+	// 
+	//	* * * * * * * * * * * Attributes * * * * * * * * * * * * //
+	// 
+	// --------------------------------------------------------- //
+	String firstName;
+	String lastName;
+	LocalDate DOB;
+	int heightInInches;
+	String gender;
+	
+	
+	// --------------------------------------------------------- //
+	// 
+	//	* * * * * * * * * * * Constructor * * * * * * * * * * *  //
+	// 
+	// --------------------------------------------------------- //
+	
+	public DriversLicense() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	// --------------------------------------------------------- //
+	// 
+	//	* * * * * * * * * * * Methods * * * * * * * * * * * * *  //
+	// 
+	// --------------------------------------------------------- //	
+	
+	
+					// 		-------------
+					// 		** GETTERS **
+					// 		-------------
+	
+	// returns fullname and Age in years
+	public String getNameAndAge() {
+		return ("Full name: " + firstName + " " + lastName + "\r\nAge: " + getAge());
+	}
+	
+	
+	public String getFullName() {
+		return (firstName + " " + lastName);
+	}
+	
+	public int getAge() {
+		return (Period.between(DOB, LocalDate.now()).getYears());
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public LocalDate getDOB() {
+		return DOB;
+	}
+
+	// returns heights after parsing inches into Feet + Inche's
+	public int getHeightInInches() {
+		return heightInInches;
+	}
+
+	public String getGender() {
+		return gender;
+	}	
+	
+					// 		-------------
+					// 		** SETTERS **
+					// 		-------------
+	
+	// sets FIRST Name
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	// sets LAST Name
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	// sets Date Of Birth using LocalDate MM DD YYYY format
+	public void setDOB(int month, int day, int year) {
+		DOB = LocalDate.of(year, month, day);
+	}
+
+	// sets height in INCHES
+	public void setHeightInInches(int height) {
+		this.heightInInches = height;
+	}
+
+	// sets gender
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
 }
--- DriversLicense/src/LicenseProgram.java
+++ DriversLicense/src/LicenseProgram.java
@@ -1,9 +1,26 @@
-
 public class LicenseProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//first instance
+		DriversLicense firstPerson = new DriversLicense();
+		firstPerson.setDOB(5, 1, 1962);
+		firstPerson.setFirstName("Ben");
+		firstPerson.setLastName("Jamin");	
+		// the "\r\n" is to include a carriage return
+		System.out.println("--First Entry-- \r\n" + firstPerson.getNameAndAge() + "\r\n");
+		
+		//second instance
+		DriversLicense secondPerson = new DriversLicense();
+		secondPerson.setDOB(11, 15, 1990);
+		secondPerson.setFirstName("Santa");
+		secondPerson.setLastName("Claus");
+		System.out.println("--Second Entry-- \r\n" + secondPerson.getNameAndAge() + "\r\n");
+		
+		// third instance
+		DriversLicense thirdPerson = new DriversLicense();
+		thirdPerson.setDOB(9, 20, 1984);
+		thirdPerson.setFirstName("Finding");
+		thirdPerson.setLastName("Nemo");
+		System.out.println("--Third Entry-- \r\n" + thirdPerson.getNameAndAge() + "\r\n");
 	}
-
 }
