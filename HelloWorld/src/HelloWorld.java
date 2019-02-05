8e76a67cc7d738392e1b0cc4d333463f193f7d4f
Abbas Musabji <amusabji@gmail.com>
2019-02-05 14:54:40 -0600

    update HelloWorld to meet reqs

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
-		System.out.println("My name's Abbas, what's your name?");
+		System.out.println("Hi, I am Abbas, what is your name?");
+		// need this to read user-generated input
 		Scanner reader = new Scanner(System.in);
+		// saving user-generated input to be used later
 		String input = reader.nextLine();
-		System.out.println("Hi " + input + ", hope you are doing well!");
+		// printing out to console custom text + user-generated input
+		System.out.println("Hi " + input + ", hope you are doing well.");
+		// we no longer need to catch user-generated input, so we close the reader
 		reader.close();
 	}
 
